dd01afeb9d92b697a2146ff9e08e270da1821c56
陈天刚 <tiangangchen@xinmoney.cn>
2019-07-17 14:32:12 +0800

    add readme

---
+++ s3/README.md
@@ -0,0 +1,20 @@
+chentiangangdeMacBook-Pro:s3 chentiangang$ ./s3 -h 
+NAME:
+   AWS S3 Client Tools; get, put, list. - A new cli application
+
+USAGE:
+   s3 [global options] command [command options] [arguments...]
+
+VERSION:
+   0.0.0
+
+COMMANDS:
+     put      Upload file to AWS S3.
+     get      Download file from AWS S3.
+     list     List Object from AWS S3.
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h     show help
+   --version, -v  print the version
+
---
+++ s3/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/chentiangang/opsTools/s3/get"
+	"github.com/chentiangang/opsTools/s3/list"
+	"github.com/chentiangang/opsTools/s3/put"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "AWS S3 Client Tools; get, put, list."
+	app.Commands = []cli.Command{
+
+		{
+			Name:  "put",
+			Usage: "Upload file to AWS S3.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "src,s",
+					Usage: "指定源文件路径",
+				},
+
+				cli.StringFlag{
+					Name:  "dest,d",
+					Usage: "指定目标文件位置",
+				},
+				cli.StringFlag{
+					Name:  "bucket,b",
+					Usage: "指定bucket",
+				},
+				cli.StringFlag{
+					Name:  "region",
+					Usage: "指定 AWS S3所在区域",
+					Value: "ap-northeast-1",
+				},
+				cli.BoolFlag{
+					Name:  "verbose,v",
+					Usage: "显示详细日志输出",
+				},
+			},
+			Action: put.Put,
+		},
+		{
+			Name:  "get",
+			Usage: "Download file from AWS S3.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "file,f",
+					Usage: "指定要下载的文件路径",
+				},
+
+				cli.StringFlag{
+					Name:  "bucket,b",
+					Usage: "指定bucket",
+					Value: "55-code-store",
+				},
+				cli.StringFlag{
+					Name:  "region",
+					Usage: "指定AWS S3所在区域",
+					Value: "ap-northeast-1",
+				},
+			},
+			Action: get.Get,
+		},
+		{
+			Name:   "list",
+			Usage:  "List Object from AWS S3.",
+			Action: list.List,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "bucket,b",
+					Usage: "指定bucket",
+					Value: "55-code-store",
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
